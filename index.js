12b182c54745db6e6ecd7238426b6be9317cff6c
Mandy Alford <mandyalford@gmail.com>
2019-12-06 11:24:20 -0700

    Add functionality to crying button

--- index.js
+++ index.js
@@ -10,12 +10,9 @@ var sillyResponses= ['Interesting response', 'Sounds ... good?', 'Me too.'];
 var cryingResponses= ['Do you want to talk?', 'Keep your head up', 'We can cry together!', 'What can I do?']
 
 
-
-var index3= Math.floor(Math.random() * cryingResponses.length);
-
 happyBtn.addEventListener('click', generateHappyResponse);
 sillyBtn.addEventListener('click', generateSillyResponse);
-// cryingBtn.addEventListener('click', generateResponse);
+cryingBtn.addEventListener('click', generateCryingResponse);
 
 function generateHappyResponse(event){
   var index= Math.floor(Math.random() * happyResponses.length);
@@ -25,4 +22,9 @@ function generateHappyResponse(event){
 function generateSillyResponse(event){
   var index= Math.floor(Math.random() * sillyResponses.length);
     messageSlot.innerText= sillyResponses[index];
+}
+
+function generateCryingResponse(event){
+  var index= Math.floor(Math.random() * cryingResponses.length);
+    messageSlot.innerText= cryingResponses[index];
 }
